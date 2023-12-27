e483d9058d30b6aa432e1c21168607a5e7a8dd1f
Bruno Vieira <brunovws77@gmail.com>
2023-12-27 16:02:42 -0300

    Banco de dados criado.

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin  # type: ignore
 from django.urls import path, include  # type: ignore
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
