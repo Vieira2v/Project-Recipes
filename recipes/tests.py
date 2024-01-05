4527b60c935b58746eb9f30f1a7d615f1526adfe
Bruno Vieira <brunovws77@gmail.com>
2024-01-05 17:19:42 -0300

    Testes nas URLs realiazdos.

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase  # type: ignore # noqa: F401
+from django.urls import resolve, reverse  # type: ignore # noqa: F401
 
 # Todos os tests ficaram dentro da classe abaixo.
 
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
+
