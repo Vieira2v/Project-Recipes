6d0960f68c9cfa29ec1771c122552ea6519ec3d7
Bruno Vieira <brunovws77@gmail.com>
2024-01-31 21:53:39 -0300

    Testes realizados nos métodos 'detail_template' e 'category'.

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -47,6 +47,17 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))  # noqa: E501
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        # Crio uma receita.
+        self.make_recipe(title=needed_title)
+        # Vou executar o teste/URL agora.
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        # Vou converter a resposta da minha URL em uma string.
+        content = response.content.decode('utf-8')
+        # Agora vou checar se 'needed_title' está no meu conteúd
+        self.assertIn(needed_title, content)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -54,3 +65,14 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000}))  # noqa: E501
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipes(self):
+        needed_title = 'This is a detail page - It load one recipe'
+        # Crio uma receita.
+        self.make_recipe(title=needed_title)
+        # Vou executar o teste/URL agora.
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        # Vou converter a resposta da minha URL em uma string.
+        content = response.content.decode('utf-8')
+        # Agora vou checar se 'needed_title' está no meu conteúd
+        self.assertIn(needed_title, content)
