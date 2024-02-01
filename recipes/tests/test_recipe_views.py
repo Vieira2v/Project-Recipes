4d53044ba9394b21570ff0bc661a2c2fecee3bb0
Bruno Vieira <brunovws77@gmail.com>
2024-01-31 22:29:31 -0300

    Realizei testes da minha função 'is_pulished' em todas as pagínas.

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -39,9 +39,31 @@ class RecipeViewsTest(RecipeTestBase):
 # Se o meu numero de receitas for diferente de 1, meu teste ira dar erro.
         self.assertEqual(len(response_context_recipes), 1)
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        # Test recipe is_published False dont show.
+        self.make_recipe(is_published=False)
+        # Vou executar o teste/URL agora.
+        response = self.client.get(reverse('recipes:home'))
+        # Agora vou checar se está aparecendo esta frase abaixo qnd o
+        # is_published estiver False.
+        self.assertIn(
+            '<h1>No recipes found here... 👎 </h1>',
+            response.content.decode('utf-8')
+        )
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1000}))  # noqa: E501
         self.assertIs(view.func, views.category)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        # Test recipe is_published False dont show.
+        recipe = self.make_recipe(is_published=False)
+        # Vou executar o teste/URL agora.
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+        # Agora vou checar se está aparecendo este erro abaixo qnd o
+        # is_published estiver False.
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))  # noqa: E501
@@ -55,7 +77,7 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', args=(1,)))
         # Vou converter a resposta da minha URL em uma string.
         content = response.content.decode('utf-8')
-        # Agora vou checar se 'needed_title' está no meu conteúd
+        # Agora vou checar se 'needed_title' está no meu conteúdo
         self.assertIn(needed_title, content)
 
     def test_recipe_detail_view_function_is_correct(self):
@@ -76,3 +98,13 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
         # Agora vou checar se 'needed_title' está no meu conteúd
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        # Test recipe is_published False dont show.
+        recipe = self.make_recipe(is_published=False)
+        # Vou executar o teste/URL agora.
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+        # Agora vou checar se está aparecendo este erro abaixo qnd o
+        # is_published estiver False.
+        self.assertEqual(response.status_code, 404)
