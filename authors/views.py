45f40b8b4f0c1d770d2f58589dbe14d13db9741b
Bruno Vieira <brunovws77@gmail.com>
2024-03-06 21:38:43 -0300

    Formulário de registros atualizados.

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render  # type: ignore # noqa: F401
+from django.shortcuts import render, redirect  # type: ignore # noqa: F401
 from .forms import RegisterForm
+from django.http import Http404  # type: ignore # noqa: F401
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)  # noqa F841
+    form = RegisterForm()
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)  # noqa F841
+
+    return redirect('authors:register')
