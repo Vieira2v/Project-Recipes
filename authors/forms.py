06a22a9dcbf71296411ce6694274782fa8b0a2f9
Bruno Vieira <brunovws77@gmail.com>
2024-03-13 19:29:49 -0300

    Organização de arquivos.

--- authors/forms.py
+++ authors/forms.py
@@ -108,6 +108,17 @@ class RegisterForm(forms.ModelForm):
             'password': 'Password',
         }
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exist = User.objects.filter(email=email).exists()
+
+        if exist:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+
+        return email
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect  # type: ignore # noqa: F401
 from .forms import RegisterForm
 from django.http import Http404  # type: ignore # noqa: F401
 from django.contrib import messages  # type: ignore # noqa: F401
+from django.urls import reverse  # type: ignore # noqa: F401
 
 
 def register_view(request):
@@ -9,6 +10,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
 
 
@@ -21,7 +23,13 @@ def register_create(request):
     form = RegisterForm(POST)  # noqa: F841
 
     if form.is_valid():
-        form.save()
+        # aqui se o form for valido eu não vou salvar a senha do usuario
+        # na base de dados, por isso o commit=False
+        user = form.save(commit=False)
+        # e aqui a password vai ficar salva no banco de dados, mas
+        # vai estar criptografada.
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
 
         del (request.session['register_form_data'])
