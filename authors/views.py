d17be77d93c44ae0d1c398e9c9b6e427eba19da4
Bruno Vieira <brunovws77@gmail.com>
2024-03-26 22:08:23 -0300

    Button para deletar receita.

--- authors/views.py
+++ authors/views.py
@@ -168,3 +168,24 @@ def dashboard_recipe_new(request):
             'form_action': reverse('authors:dashboard_recipe_new')
         }
     )
+
+
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    id = POST.get('id')
+
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+
+    recipe.delete()
+    messages.success(request, 'Deleted successfully.')
+    return redirect(reverse('authors:dashboard'))
